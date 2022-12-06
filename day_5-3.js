6a8598841dbf7b99b22a0c68391ae073a3a8fca4
json-0201 <106159207+json-0201@users.noreply.github.com>
2022-12-06 14:01:35 -0800

    Create day_5-3.js

---
+++ day_5-3.js
@@ -0,0 +1,17 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    return nums.map(ele => ele%2==0 ? ele*2 : ele*3);
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
