b8526aefc7c06da4f4b07ff6240ca1aed39ba1f0
json-0201 <106159207+json-0201@users.noreply.github.com>
2022-12-06 13:39:43 -0800

    Create day_5-2.js

---
+++ day_5-2.js
@@ -0,0 +1,29 @@
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    let area = expressions[0];
+    let perimeter = expressions[1];
+    var s1 = (perimeter + Math.sqrt(perimeter**2 - 16*area))/4;
+    var s2 = (perimeter - Math.sqrt(perimeter**2 - 16*area))/4;
+    return ([s2, s1]);
+}
+
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
