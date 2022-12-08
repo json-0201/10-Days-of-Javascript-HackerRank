431d9f4be961465c688ecb45fc2df4161228c20c
json-0201 <ghks940201@ucla.edu>
2022-12-08 11:47:08 -0800

    Create day_7-2.js

---
+++ day_7-2.js
@@ -0,0 +1,22 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    let exp = /^(Mr|Mrs|Ms|Dr|Er)\.\w+$/
+    const re = new RegExp(exp);
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
